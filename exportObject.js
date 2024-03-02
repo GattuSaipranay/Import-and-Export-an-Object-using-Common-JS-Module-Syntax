e5111010a2c376eb4f638552b089c4a308c8f96a
GattuSaipranay <saipranaysrinivas@gmail.com>
2024-03-02 11:20:47 +0000

    Import and Export an Object using Common JS Module Syntax

---
+++ exportObject.js
@@ -0,0 +1,8 @@
+// Define the object to export
+const person = {
+    firstName: "John",
+    lastName: "Wilson"
+};
+
+// Export the object using default export syntax
+module.exports = person;
---
+++ importObject.js
@@ -0,0 +1,5 @@
+// Import the default export
+const person = require("./exportObject");
+
+// Test the imported object
+console.log("Imported object:", person);
